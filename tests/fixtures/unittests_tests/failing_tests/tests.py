972ef13ceff1219085fdd7766a820d3a6de704a3
Marcello Krahforst <marcello.2001@hotmail.com>
2025-02-11 15:08:53 +0100

    fix(#28): simplify tests
    Removes one test to simplify the tests to keep them minimal

--- tests/fixtures/unittests_tests/failing_tests/tests.py
+++ tests/fixtures/unittests_tests/failing_tests/tests.py
@@ -5,9 +5,6 @@ class TestTests(unittest.TestCase):
     def test_this_should_fail_to_be_true(self):
         self.assertTrue(False)
 
-    def test_this_should_fail_to_be_false(self):
-        self.assertFalse(True)
-
     def test_string__does_not_contain_substring(self):
         self.assertIn("substrings", "this string contains a substring")
 
--- tests/fixtures/unittests_tests/successful_tests/tests.py
+++ tests/fixtures/unittests_tests/successful_tests/tests.py
@@ -5,9 +5,6 @@ class TestTests(unittest.TestCase):
     def test_this_should_be_true(self):
         self.assertTrue(True)
 
-    def test_this_should_be_false(self):
-        self.assertFalse(False)
-
     def test_string_contains_substring(self):
         self.assertIn("substring", "this string contains a substring")
 
