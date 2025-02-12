99b22e4e0665eca10a559830fe43a8e73a0760fb
Johan <104634655+joel90688@users.noreply.github.com>
2025-02-12 14:29:36 +0100

    feat(#26): feature and tests for write_job_logs added (#35)
    * feat(#25): feature and tests for get_job_logs added
    
    * fix(#25): changed from ephemeral to fixtures in testing
    
    * feat(#26): Added funcitonality for write_job_logs() with tests
    
    * fix(#26): removed file_name parameter
    
    * fix(#26): removed old files
    
    * fix(#26): addressed pr comments
    
    ---------
    
    Co-authored-by: Johan Nilsson <ni5@ug.kth.se>

--- tests/test_logs.py
+++ tests/test_logs.py
@@ -10,9 +10,31 @@ class TestUtils(unittest.TestCase):
     # Set up the test environment
     def setUp(self):
         self.fixture_folder = "tests/fixtures/log_tests"
+        self.ephemeral_folder = "temp/log_test/"
+
+    def tearDown(self):
+        if check_if_folder_exists(self.ephemeral_folder):
+            remove_folder(self.ephemeral_folder)
+
+    # Function for creating mock JobMetadata
+    def mock_metadata(self, id: str) -> JobMetadata:
+        return JobMetadata(
+            id=id,
+            status="success",
+            repo_url="https://github.com/dd2480-spring-2025-group-1/assignment-1",
+            ref="main",
+            head_commit="b7f1a1c",
+            author="Strengthless",
+            time_started=23,
+            time_ended=23,
+            logs=["Mocked log 1", "Mocked log 2", "Mocked log 3", "Mocked log 4"],
+        )
 
     # Tests for get_job_logs
     def test_get_job_logs(self):
+        """
+        Test getting multiple log ids and verifying what is returned.
+        """
         result = get_job_logs(os.path.join(self.fixture_folder, "multiple_log"))
 
         self.assertIn("ad21", result)
@@ -20,10 +42,38 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(len(result), 2)
 
     def test_get_job_log(self):
+        """
+        Test getting a single log id and verifying what has been returned.
+        """
         result = get_job_logs(os.path.join(self.fixture_folder, "single_log"))
 
         self.assertIn("ad21", result)
         self.assertEqual(len(result), 1)
+
+    # Tests for write_job_logs
+    def test_write_job_logs(self):
+        """
+        Test writing multiple logs to a file and verifying they can be read via get_job_logs().
+        """
+        mock1 = self.mock_metadata("ad21")
+        mock2 = self.mock_metadata("df44")
+
+        write_job_log("ad21", mock1, self.ephemeral_folder)
+        write_job_log("df44", mock2, self.ephemeral_folder)
+
+        ids = get_job_logs(self.ephemeral_folder)
+        self.assertListEqual(ids, ["ad21", "df44"])
+
+    def test_write_job_log(self):
+        """
+        Test writing a single log and verifying its contents via read_job_log().
+        """
+        mock = self.mock_metadata("ad21")
+
+        write_job_log("ad21", mock, self.ephemeral_folder)
+
+        ids = get_job_logs(self.ephemeral_folder)
+        self.assertListEqual(ids, ["ad21"])
 
 
 if __name__ == "__main__":
